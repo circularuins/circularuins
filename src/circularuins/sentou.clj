b12f3680dd4c8c014797d2317d278fb45d3ae2cd
和気 夏彦 <wake@air.local>
2014-07-15 03:18:10 +0900

    2次元配列データからmap-seqをつくるようにした

--- src/circularuins/sentou.clj
+++ src/circularuins/sentou.clj
@@ -1,16 +1,17 @@
 (ns circularuins.sentou)
 
-(def gokuu [["777/7/7" 100]
-            ["778/7/8" 120]])
-(def sensi ["gokuu"])
+(def sensi-data [["gokuu" [["777/7/7" 100]
+                           ["778/7/8" 120]]]
+                 ["pikkoro" [["666/6/6" 90]
+                             ["667/6/7" 300]]]])
 
 (defn make-map-seq [data]
   (map #(array-map :age (get % 0) :power (get % 1)) data))
 
-(defn sensi-map [name data]
-  (array-map (keyword name) data))
+(defn sensi-map [data]
+  (map #(array-map (keyword (get % 0)) (make-map-seq (get % 1))) data))
 
-(def zsensi (sensi-map "gokuu" (make-map-seq gokuu)))
+(def zsensi (sensi-map sensi-data))
 
 (defn all-sensi []
   zsensi)
