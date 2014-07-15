284c07d9832eb4677ff9fe711b7af3464a13f6ba
和気 夏彦 <wake@air.local>
2014-07-15 17:44:03 +0900

    sentouをd3.jsで表示できるようにした

--- src/circularuins/sentou.clj
+++ src/circularuins/sentou.clj
@@ -1,15 +1,15 @@
 (ns circularuins.sentou)
 
 (def sensi-data [["gokuu" [["777/7/7" 100]
-                           ["778/7/8" 120]]]
-                 ["pikkoro" [["666/6/6" 90]
-                             ["667/6/7" 300]]]])
+                           ["778/7/8" 1120]]]
+                 ["pikkoro" [["777/7/15" 105]
+                             ["778/6/7" 820]]]])
 
 (defn make-map-seq [data]
   (map #(array-map :age (get % 0) :power (get % 1)) data))
 
 (defn sensi-map [data]
-  (map #(array-map (keyword (get % 0)) (make-map-seq (get % 1))) data))
+  (apply merge (map #(hash-map (keyword (get % 0)) (make-map-seq (get % 1))) data)))
 
 (def zsensi (sensi-map sensi-data))
 
